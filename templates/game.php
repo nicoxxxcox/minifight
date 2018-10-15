5823aa71d4f66f3fe4c0dfc0bc75afac41c2c450
nicolas.grissi <nicolas.grissi@edenwork.com>
2018-10-15 17:36:03 +0200

    intégration de la maquette terminé

--- templates/game.php
+++ templates/game.php
@@ -1,29 +1,40 @@
-<div class="row"><a class="btn" href="?deconnexion=1"><span class="pill pill-deconnexion ">Déconnexion</span></a></div>
 <div class="row">
+    <div class="col-md-10 mx-auto">
+        <img class="mr-1" src="./assets/info.svg" alt="information"><span class="game-info"> Cliquez sur un soldat pour le frapper ! </span>
+        <div class="row">
+            <div class="col">
 
-    <fieldset class="fieldset-info">
-        <legend>Mes informations</legend>
-        <div class="pill-form pill">Moi : <?= htmlspecialchars($perso->getNom()) ?>
-            <span id="icn" class="icn-life">  <?= $perso->getVie() ?></span>
-        </div>
-    </fieldset >
-    <fieldset class="fieldset-frapper">
-        <legend>Qui frapper ?</legend>
-        <p>
-            <?php
-            $persos = $manager->getList(/*$perso->getNom()*/);
-            if (empty($persos)) {
-                echo 'Personne à frapper !';
-            } else {
-                foreach ($persos as $unPerso) {
-                    echo '
-                <div class="pill-form pill ">
-                    <i class="fas fa-child"></i> 
+                <div class="row my-3 pers-line">
+                    <div class="col-md-2">
+                        <img src="./assets/viking.svg" alt="">
+                    </div>
+                    <div class="col-md-10">
+                        <div class="pers-name"><?= htmlspecialchars($perso->getNom()) ?></div>
+                        <div class="pers-life">Vie : <?= $perso->getVie() ?> <img src="./assets/heart.svg" alt=""></div>
+                    </div>
+                </div>
+
+                <div class="row mb-5">
+                    <div class="col mt-3">
+                        <?php
+                        $persos = $manager->getList(/*$perso->getNom()*/);
+                        if (empty($persos)) {
+                            echo 'Personne à frapper !';
+                        } else {
+                            foreach ($persos as $unPerso) {
+                                echo '
+                <div class="btn  btn-pers mr-2 mb-3 ">
+                   
                     <a href="?frapper=' . $unPerso->getId() . '">  ' . htmlspecialchars($unPerso->getNom()) . '   <span class="icn-life"> ' . $unPerso->getVie() . '</span></a><br />
                 </div>';
-                }
-            }
-            ?>
-        </p>
-    </fieldset>
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 </div>
